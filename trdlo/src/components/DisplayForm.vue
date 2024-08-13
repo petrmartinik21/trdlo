f1fa61435f7193910f94a222c32e4d3d63f79261
petrmartinik21 <165170922+petrmartinik21@users.noreply.github.com>
2024-08-13 10:59:28 +0200

    added edit forl all areas and commented cancel button

--- trdlo/src/components/DisplayForm.vue
+++ trdlo/src/components/DisplayForm.vue
@@ -7,15 +7,38 @@
             v-model="item.name" 
             v-else
             :disabled="!isEditing[index]"
-        />
+            />
         </p>
-        <p>Station: {{ item.station }}</p>
-        <p>Description: {{ item.description }}</p>
-        <p>Solution: {{ item.solution }}</p>
+        <p>Station: 
+            <span v-if="!isEditing[index]">{{ item.station }}</span>
+            <input type="text" 
+            v-model="item.station" 
+            v-else
+            :disabled="!isEditing[index]"
+            />
+        </p>
+        <p>Description: 
+            <span v-if="!isEditing[index]">{{ item.description }}</span>
+            <textarea type="text" 
+            v-model="item.description" 
+            v-else
+            :disabled="!isEditing[index]"
+            >
+            </textarea>
+        </p>
+        <p>Solution:  
+            <span v-if="!isEditing[index]">{{ item.solution }}</span>
+            <textarea type="text" 
+            v-model="item.solution" 
+            v-else
+            :disabled="!isEditing[index]"
+            >
+            </textarea>
+        </p>
 
         <button @click="isEditing[index] = true" v-if="!isEditing[index]">Edit</button>
-        <button @click="saveChanges(index)" v-else>Save</button>
-        <button @click="cancelChanges(index)" v-else>Cancel</button>
+        <button @click="saveChanges(index)" v-if="isEditing[index]">Save</button>
+        <!-- <button @click="cancelChanges(index)" v-else >Cancel</button> -->
     </li>
 </ul>
         <p v-else>No data submitted yet.</p>
@@ -36,7 +59,9 @@ const props = defineProps({
 
 const emit = defineEmits(["save-changes"]);
 const isEditing = ref(new Array(props.data.length).fill(false));
+
 console.log(isEditing);
+
 
 
 function saveChanges(index) {
@@ -44,10 +69,10 @@ function saveChanges(index) {
   isEditing.value[index] = false;
 }
 
-function cancelChanges(index) {
-  // Revert changes (e.g., reload data from the original source)
-  isEditing.value[index] = false;
-}
+// function cancelChanges(index) {
+//   // Revert changes (e.g., reload data from the original source)
+//   isEditing.value[index] = false;
+// }
 
 
 
