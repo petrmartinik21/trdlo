ea16fb40bade20b5185f6dc7f07500c134caaa00
petrmartinik21 <165170922+petrmartinik21@users.noreply.github.com>
2024-08-20 11:00:31 +0200

    set node .env, database/seed.js, inserting data in supabase

---
+++ trdlo/database/seed.js
@@ -0,0 +1,35 @@
+/* eslint-env node */
+
+
+import { fakerCS_CZ as faker} from '@faker-js/faker'
+
+// const personName = faker.person.fullName()
+// const personBio = faker.person.bio()
+// console.log(personName)
+// console.log(personBio)
+
+import { createClient } from '@supabase/supabase-js'
+
+// Create a single supabase client for interacting with your database
+const supabase = createClient( 
+    process.env.VITE_SUPABASE_URL,
+    process.env.SERVICE_ROLE_KEY
+)
+
+
+const seedTruloTwo = async () => {
+    
+    const name =  faker.lorem.words(3);
+    
+    await supabase.from('trdlotwo').insert({
+        name: name,
+        station: name.toLocaleLowerCase().replace(/ /g, '-'),
+        description: faker.lorem.paragraph(),
+        solution: faker.lorem.paragraph(),
+        status: faker.helpers.arrayElement(['in-progress', 'completed']),
+        collaborators: faker.helpers.arrayElements([1,2,3])
+    })
+}
+
+await seedTruloTwo()
+
--- trdlo/src/components/lib/supabaseClient.js
+++ trdlo/src/components/lib/supabaseClient.js
@@ -2,11 +2,9 @@
 import { createClient } from '@supabase/supabase-js'
 
 // Create a single supabase client for interacting with your database
-export const supabase = createClient(
-   
-   
-import.meta.env.VITE_SUPABASE_URL,
-import.meta.env.VITE_SUPABASE_KEY
+export const supabase = createClient( 
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_KEY
 )
 
 
