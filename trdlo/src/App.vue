95e59ab62987952c9741c80c55d008efc0fde0bd
petrmartinik21 <165170922+petrmartinik21@users.noreply.github.com>
2024-09-02 13:23:18 +0200

    added priority and save it to database

--- trdlo/src/App.vue
+++ trdlo/src/App.vue
@@ -1,89 +1,83 @@
 <script setup>
-import InputForm from './components/InputForm.vue';
-import DisplayForm from './components/DisplayForm.vue';
+import InputForm from './components/InputForm.vue'
+import DisplayForm from './components/DisplayForm.vue'
 import { ref, onMounted } from 'vue'
 import { supabase } from './components/lib/supabaseClient'
 
-
-const receivedData = ref([ ]);
+const receivedData = ref([])
 
 // This function is called when the form is submitted or onMounted
 // ****************************************************************
 async function fetchData() {
   try {
-    const { data, error } = await supabase
-      .from('trdlotwo') 
-      .select('*'); 
-
+    const { data, error } = await supabase.from('trdlotwo').select('*')
+    // console.log( typeof data[2].importance)
     if (error) {
-      console.error('Error fetching data:', error);
+      console.error('Error fetching data:', error)
     } else {
-      receivedData.value = data; 
+      receivedData.value = data
     }
   } catch (error) {
-    console.error('Error during Supabase operation:', error);
+    console.error('Error during Supabase operation:', error)
   }
 }
 
 onMounted(() => {
-  fetchData(); 
-});
+  fetchData()
+})
 // ***********************************************************
 
 // This function insert and return data from supabase
 // ************************************************************
 async function handleDataSubmitted(data) {
-    try {
-      const { data: insertedData, error } = await supabase
-      .from('trdlotwo')
-      .insert(data) 
-      .select()
-      
+  try {
+    const { data: insertedData, error } = await supabase.from('trdlotwo').insert(data).select()
+
     if (error) {
-        console.error('Error inserting data:', error);
+      console.error('Error inserting data:', error)
     } else {
-      receivedData.value.push(...insertedData);
+      receivedData.value.push(...insertedData)
     }
   } catch (error) {
-    console.error('Error during Supabase operation:', error);
+    console.error('Error during Supabase operation:', error)
   }
 }
 // ************************************************************
-    
 
 // This function check if all required data are inserted
 // ************************************************************
-const isFormValid = ref(false);
+const isFormValid = ref(false)
 const updateFormValidity = (isValid) => {
-    isFormValid.value = isValid;
+  isFormValid.value = isValid
 }
 // ************************************************************
 
 //  This function update the data in supabase
 // ************************************************************
 async function updatedData(update) {
-    try {
-      const { error } = await supabase
+  try {
+    const { error } = await supabase
       .from('trdlotwo')
       .update({
         name: update.name,
         station: update.station,
         description: update.description,
-        solution: update.solution
-      }) 
+        solution: update.solution,
+        importance: update.importance
+      })
       .eq('id', update.id)
 
     if (error) {
-        console.error('Error updating data:', error);
-        // this else update the data in the array receivedData
+      console.error('Error updating data:', error)
+      // this else update the data in the array receivedData
     } else {
-      const itemIndex = receivedData.value.findIndex(item => item.id === update.id);
+      const itemIndex = receivedData.value.findIndex((item) => item.id === update.id)
       if (itemIndex !== -1) {
-        receivedData.value[itemIndex] = update;
+        receivedData.value[itemIndex] = update
       }
     }
   } catch (error) {
-    console.error('Error during Supabase operation:', error);
+    console.error('Error during Supabase operation:', error)
   }
 }
 // **************************************************************
@@ -92,58 +86,47 @@ async function updatedData(update) {
 // **************************************************************
 async function deleteData(id) {
   try {
-    const { error } = await supabase
-    .from('trdlotwo')
-    .delete()
-    .eq('id', id)
+    const { error } = await supabase.from('trdlotwo').delete().eq('id', id)
 
-if (error) {
-    console.error('Error deleting data:', error);
-} else {
-  console.log(`Block id:${id} deleted successfully`);
-  // this else delete the data in the array receivedData
-  const index = receivedData.value.findIndex(item => item.id === id);
-    if (index !== -1) {
-      receivedData.value.splice(index, 1);
+    if (error) {
+      console.error('Error deleting data:', error)
+    } else {
+      console.log(`Block id:${id} deleted successfully`)
+      // this else delete the data in the array receivedData
+      const index = receivedData.value.findIndex((item) => item.id === id)
+      if (index !== -1) {
+        receivedData.value.splice(index, 1)
+      }
     }
+  } catch (error) {
+    console.error('Error during Supabase operation:', error)
   }
-} catch (error) {
-  console.error('Error during Supabase operation:', error);
-}
 }
 // ****************************************************************
-
-
-
-
-
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
   </header>
 
-<InputForm @data-submitted="handleDataSubmitted" @formValidityChanged="updateFormValidity">
-    <template v-slot:icon >
-      <span v-if="!isFormValid.value" >✍️</span> <span v-else>👏</span>
+  <InputForm @data-submitted="handleDataSubmitted" @formValidityChanged="updateFormValidity">
+    <template v-slot:icon>
+      <span v-if="!isFormValid.value">✍️</span> <span v-else>👏</span>
       Save:
     </template>
 
-    <template #fill-the-form><h2>Fill all the fields to submit the form: <strong>{{ isFormValid.value ? '✅' : '❌' }}</strong></h2></template>
-    
-</InputForm>
+    <template #fill-the-form
+      ><h2>
+        Fill all the fields to submit the form:
+        <strong>{{ isFormValid.value ? '✅' : '❌' }}</strong>
+      </h2></template
+    >
+  </InputForm>
 
-<display-form :data="receivedData" 
-              @delete-data="deleteData" 
-              @save-changes="updatedData">
-</display-form>
-
+  <display-form :data="receivedData" @delete-data="deleteData" @save-changes="updatedData">
+  </display-form>
 </template>
-
-
-
 
 <style scoped>
 header {
--- trdlo/src/components/DisplayForm.vue
+++ trdlo/src/components/DisplayForm.vue
@@ -1,118 +1,111 @@
 <template>
-    <ul v-if="sortedData.length > 0">
-   <!-- <li v-for="(item, index) in sortedData" :key="item.id"> -->
-    <li v-for="(item, index) in sortedData" :key="item.id" >
-        <span>{{  item.id }}</span> 
-        <p>Name: 
-            <span v-if="!isEditing[index]">{{ item.name }}</span>
-            <input type="text" 
-            v-model="item.name" 
-            v-else
-            :disabled="!isEditing[index]"
-            />
-        </p>
-        <p>Station: 
-            <span v-if="!isEditing[index]">{{ item.station }}</span>
-            <input type="text" 
-            v-model="item.station" 
-            v-else
-            :disabled="!isEditing[index]"
-            />
-        </p>
-        <p>Description: 
-            <span v-if="!isEditing[index]">{{ item.description }}</span>
-            <textarea type="text" 
-            v-model="item.description" 
-            v-else
-            :disabled="!isEditing[index]"
-            >
-            </textarea>
-        </p>
-        <p>Solution:  
-            <span v-if="!isEditing[index]">{{ item.solution }}</span>
-            <textarea type="text" 
-            v-model="item.solution" 
-            v-else
-            :disabled="!isEditing[index]"
-            >
-            </textarea>
-        </p>
+  <ul v-if="sortedData.length > 0">
+    <!-- <li v-for="(item, index) in sortedData" :key="item.id"> -->
+    <li v-for="(item, index) in sortedData" :key="item.id">
+      <span>{{ item.id }}</span>
+      <p>
+        Name:
+        <span v-if="!isEditing[index]">{{ item.name }}</span>
+        <input type="text" v-model="item.name" v-else :disabled="!isEditing[index]" />
+      </p>
+      <p>
+        Station:
+        <span v-if="!isEditing[index]">{{ item.station }}</span>
+        <input type="text" v-model="item.station" v-else :disabled="!isEditing[index]" />
+      </p>
+      <p>
+        Description:
+        <span v-if="!isEditing[index]">{{ item.description }}</span>
+        <textarea type="text" v-model="item.description" v-else :disabled="!isEditing[index]">
+        </textarea>
+      </p>
+      <p>
+        Solution:
+        <span v-if="!isEditing[index]">{{ item.solution }}</span>
+        <textarea type="text" v-model="item.solution" v-else :disabled="!isEditing[index]">
+        </textarea>
+      </p>
 
-        <button @click="isEditing[index] = true" v-if="!isEditing[index]">Edit</button>
-        <button @click="saveChanges(item.id)" v-if="isEditing[index]">Save</button>
-        <button @click="deleteData(item.id)" >Delete</button>
+      <p>
+        Importance:
+        <span v-if="!isEditing[index]">{{ item.importance }}</span>
+        <select v-model="item.importance" v-else :disabled="!isEditing[index]">
+          <option value="low">Low</option>
+          <option value="middle">Middle</option>
+          <option value="high">High</option>
+        </select>
+      </p>
+
+      <button @click="isEditing[index] = true" v-if="!isEditing[index]">Edit</button>
+      <button @click="saveChanges(item.id)" v-if="isEditing[index]">Save</button>
+      <button @click="deleteData(item.id)">Delete</button>
     </li>
-</ul>
-        <p v-else>No data submitted yet.</p>
-  
+  </ul>
+  <p v-else>No data submitted yet.</p>
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'   
+import { ref, computed } from 'vue'
 import { defineProps, defineEmits } from 'vue'
 
 // Data from parent component
 // *******************************************************
 const props = defineProps({
-    data: {
-        type: Array,
-        required: true
-    }
-});
+  data: {
+    type: Array,
+    required: true
+  }
+})
 // *******************************************************
-const emit = defineEmits(["save-changes", "delete-data"]);
-const isEditing = ref(new Array(props.data.length).fill(false));
+const emit = defineEmits(['save-changes', 'delete-data'])
+const isEditing = ref(new Array(props.data.length).fill(false))
 
 // Computed property to sort data in descending order
 // *******************************************************
 const sortedData = computed(() => {
   return [...props.data].sort((a, b) => {
-    return b.id - a.id; 
-  });
-});
+    return b.id - a.id
+  })
+})
 // *******************************************************
-
 
 // Save changes to updatedData
 // *******************************************************
-function saveChanges (id) {
-  const updatedData = sortedData.value.find(item => item.id === id);
-     emit('save-changes', updatedData);
-     const index = sortedData.value.findIndex(item => item.id === id);
-      if (index !== -1) {
-          isEditing.value[index] = false; 
-      }
+function saveChanges(id) {
+  const updatedData = sortedData.value.find((item) => item.id === id)
+  emit('save-changes', updatedData)
+  const index = sortedData.value.findIndex((item) => item.id === id)
+  if (index !== -1) {
+    isEditing.value[index] = false
   }
+  
+}
 
 //  Delete data from supabase
 // *******************************************************
 function deleteData(id) {
-   if (confirm('Do you want to delete the data?')) {
-     emit('delete-data', id);
-   }
+  if (confirm('Do you want to delete the data?')) {
+    emit('delete-data', id)
+  }
 }
-
-
 </script>
 
 <style lang="scss" scoped>
 ul {
-    list-style-type: none;
-    background: rgb(132, 132, 136);
-    width: 35rem;
-    border: solid 2px white;
+  list-style-type: none;
+  background: rgb(132, 132, 136);
+  width: 35rem;
+  border: solid 2px white;
+  border-radius: 0.75rem;
+  margin: 1rem;
+  padding: 0.7rem;
+
+  li {
+    background: rgb(62, 62, 102);
+    margin: 0.6rem;
+    padding: 0.65rem;
     border-radius: 0.75rem;
-    margin: 1rem;
-    padding: .7rem;
-    
-    li {
-        background:rgb(62, 62, 102);
-        margin: 0.6rem;
-        padding: .65rem;
-        border-radius: 0.75rem;
-        color: rgb(224, 224, 224);          
-    }
-
+    color: rgb(224, 224, 224);
+  }
 }
-
 </style>
